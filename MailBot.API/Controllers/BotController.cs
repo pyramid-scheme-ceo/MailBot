876841ec3a31656aff089453718b9e3d0f86ec85
using_namespace_matt <matt.astach@gmail.com>
2021-07-25 10:45:17 +0930

    Initial commit

---
+++ MailBot.API/Controllers/BotController.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+
+namespace MailBot.API.Controllers
+{
+    [ApiController]
+    [Route("api/bot/messages")]
+    public class BotController : ControllerBase
+    {
+        private readonly IBotFrameworkHttpAdapter _adapter;
+        private readonly IBot _bot;
+
+        public BotController(IBotFrameworkHttpAdapter adapter, IBot bot)
+        {
+            _adapter = adapter;
+            _bot = bot;
+        }
+
+        [AllowAnonymous]
+        [HttpPost, HttpGet]
+        public async Task PostAsync()
+        {
+            await _adapter.ProcessAsync(Request, Response, _bot);
+        }
+    }
+}
